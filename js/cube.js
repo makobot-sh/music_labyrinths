4751866e690e96988359f3b41ff9ae6304e7ef98
Chiro1975 <tach.365@gmail.com>
2021-08-16 00:38:39 -0300

    Maze generator in 3D done

--- js/cube.js
+++ js/cube.js
@@ -1,14 +1,15 @@
 import * as aux from './auxiliary.js';
+
 // 0. Our Javascript will go here.
 // 1. Creating the scene
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, //FOV (in degrees)
     window.innerWidth / window.innerHeight, //Aspect ratio
     1, //Near clipping plane (objects nearer won't be rendered)
-    500 ); //Far clipping plane (object further won't be rendered)
+    700 ); //Far clipping plane (object further won't be rendered)
 
-camera.position.set( -25, 0, 75);
-//camera.lookAt( 0, 0, 0 )
+camera.position.set( 400, 700, -450);
+camera.lookAt( 400, 0, -450)
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, //Size at which we render our app (width, height)
@@ -17,44 +18,43 @@ renderer.setSize( window.innerWidth, //Size at which we render our app (width, h
     //eg: etSize(window.innerWidth/2, window.innerHeight/2, false) renders app at half resolution
 document.body.appendChild( renderer.domElement ); //<canvas> element our renderer uses to display the scene to us
 
-// 2. Adding the plane
-//yellow walls
-aux.createPlane(scene, [100,50],0xffff00, [-50,0,0], [0,90,0]);
-aux.createPlane(scene, [200,50],0xffff00, [0,0,0], [0,90,0]);
-//cyan walls
-aux.createPlane(scene, [200,50],0x00ffff, [0 - 200/2,0,-100], [0,0,0]);
-aux.createPlane(scene, [100,50],0x00ffff, [-50 -100/2,0,-50], [0,0,0]);
-//green walls
-aux.createPlane(scene, [100,50],0x00ff00, [0 -50 -100  ,0,0], [0,90,0]);
-aux.createPlane(scene, [200,50],0x00ff00, [0 -200,0,0], [0,90,0]);
-//magenta walls
-aux.createPlane(scene, [200,50],0xff00ff, [0 - 200/2,0,100], [0,0,0]);
-aux.createPlane(scene, [100,50],0xff00ff, [-50 -100/2,0,50], [0,0,0]);
-
-var tweenRot = new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed);
-var tween1 = new TWEEN.Tween( camera.position ).to( {x:"+0",y:"+0",z:"-150"}, 2000 ).onComplete(function(){
-    new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
-});
-var tween2 = new TWEEN.Tween( camera.position ).to( {x:"-150",y:"+0",z:"+0"}, 2000 ).onComplete(function(){
-    new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
-});
-var tween3 = new TWEEN.Tween( camera.position ).to( {x:"+0",y:"+0",z:"+150"}, 2000 ).onComplete(function(){
-    new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
-});
-var tween4 = new TWEEN.Tween( camera.position ).to( {x:"+150",y:"+0",z:"+0"}, 2000 ).onComplete(function(){
-    new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
-});
-
-tween1.chain(tween2);
-tween2.chain(tween3);
-tween3.chain(tween4);
-tween4.chain(tween1);
-tween1.start();
+generate_maze();
+tween_animation();
 
 
 //3. Create render/animate loop
 // This creates a loop that causes the renderer to draw the scene *every time the screen is refreshed*
 // Note: this pauses when the user navigates to another browser tab!
+animate();
+
+function tween_animation(){
+    var tweenRot = new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed);
+    var tween1 = new TWEEN.Tween( camera.position ).to( {x:"+150",y:"+0",z:"0"}, 2000 ).onComplete(function(){
+        new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
+    });
+    
+    var tween2 = new TWEEN.Tween( camera.position ).to( {x:"150",y:"+0",z:"+0"}, 2000 ).onComplete(function(){
+        new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
+    });
+    
+    var tween3 = new TWEEN.Tween( camera.position ).to( {x:"+0",y:"+0",z:"+1000"}, 20000 ).onComplete(function(){
+        new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
+    });
+
+    var tween4 = new TWEEN.Tween( camera.position ).to( {x:"+1500",y:"+0",z:"+0"}, 20000 ).onComplete(function(){
+        new TWEEN.Tween( camera.rotation ).to( {y: aux.ninetyDeg} , aux.rotSpeed).start();
+    });
+}
+
+  
+tween1.chain(tween2);
+tween2.chain(tween3);
+tween3.chain(tween4);
+tween4.chain(tween1);
+//tween1.start()
+
+}
+
 function animate() {
     requestAnimationFrame( animate );
     TWEEN.update();
@@ -66,4 +66,30 @@ function animate() {
     //camera.position.z -= 1
     renderer.render( scene, camera );
 }
-animate();
+
+
+function generate_maze(){
+    // 2. Adding the planes based in the JSON data
+    //Read the JSON file
+    fetch("../parser/data_lines.json")
+    .then(response => response.json())
+    .then(json => { 
+        var incrementador = 0;
+        //Generate the lines given by the JSON (the first two are omitted)
+        for (var key in json) {
+            incrementador = incrementador + 1
+            if (incrementador > 2){
+                let x1 = Math.floor(json[key].x1);
+                let x2 = Math.floor(json[key].x2);
+                let y1 = Math.floor(json[key].y1);
+                let y2 = Math.floor(json[key].y2);
+                if (y1 == y2){
+                    aux.createPlane(scene, [30,30],0x00ffff, [x1,0,-y1], [0,0,0]);
+                } else {
+                    aux.createPlane(scene, [30,30],0xffff00, [x2-15,0,-y2+15], [0,90,0]);
+                }
+            };
+        }
+    }
+    );
+}
