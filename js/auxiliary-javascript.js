09a1ddeb4c2264a9cdcd62082dab0a367e77570a
Chiro1975 <tach.365@gmail.com>
2021-08-29 20:49:40 -0300

    Fix the fix

--- js/auxiliary-javascript.js
+++ js/auxiliary-javascript.js
@@ -6,7 +6,7 @@ export {
   
 }
 
-var config = await getJson("./config/config.json");
+var config = await getJson("../config/config.json");
 
 function printError(error, explicit) {
   console.log(`[${explicit ? 'EXPLICIT' : 'INEXPLICIT'}] ${error.name}: ${error.message}. This error probably occurs because JSON file has a syntax error or does not exist`);
